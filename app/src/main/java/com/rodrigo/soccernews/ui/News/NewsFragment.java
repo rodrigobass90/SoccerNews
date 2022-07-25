4ca1783206085f896da84a223b4d7207b967915a
rodrigobass90 <melo-rodrigo90@hotmail.com>
2022-07-25 13:15:11 -0300

    first commit

---
+++ app/src/main/java/com/rodrigo/soccernews/ui/News/NewsFragment.java
@@ -0,0 +1,38 @@
+package com.rodrigo.soccernews.ui.News;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.rodrigo.soccernews.databinding.FragmentHomeBinding;
+import com.rodrigo.soccernews.databinding.FragmentNewsBinding;
+
+public class NewsFragment extends Fragment {
+
+    private FragmentNewsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NewsViewModel homeViewModel =
+                new ViewModelProvider(this).get(NewsViewModel.class);
+
+        binding = com.rodrigo.soccernews.databinding.FragmentFavouritesBindingBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
